87352fc6976939e7f253d73e325670af9dc15750
AntonioSoto01 <asotocid@gmail.com>
2023-10-30 13:43:04 +0100

    Fixed incorrect queries and added diagram

--- db/initial_load.sql
+++ db/initial_load.sql
@@ -1,20 +1,18 @@
 USE meetmap;
-Select *  from Activty;
+Select *  from Activity;
 SELECT Likes.*, Subscribers.*
-FROM User
-LEFT JOIN Likes ON User.id = Likes.user_id
-LEFT JOIN Subscribers ON User.id = Subscribers.user_id;
-INSERT INTO Subscribers VALUES ('id', 'id_user','id_activity');
+FROM Users
+LEFT JOIN Likes ON Users.id = Likes.user_id
+LEFT JOIN Subscribers ON Users.id = Subscribers.user_id;
+INSERT INTO Subscribers (user_id, activity_id) VALUES (1, 1);
 INSERT INTO Users (username, email)
 VALUES ('valor_del_email', 'valor_del_nombre_de_usuario');
-UPDATE Users
-SET name = 'nuevo_nombre', last_name = 'nuevo_apellido'
-WHERE username = 'nombre_de_usuario_web';
-Select * from Activity where activity_id = 'nombre';
+UPDATE Users SET name = 'nuevo_nombre', last_name = 'nuevo_apellido' WHERE username = 'nombre_de_usuario_web';
+Select * from Activity where id = 'nombre';
 SELECT *
 FROM Message
 WHERE activity_id = 'nombre actividad' ORDER BY date_time;
 INSERT INTO Message (user_id, content, date_time, activity_id)
-SELECT Users.id, 'contenido_del_mensaje', NOW(), Actividades.id
+SELECT Users.id, 'contenido_del_mensaje', NOW(), Activity.id
 FROM Users, Activity
 WHERE Users.username = 'nombre_de_usuario' AND Activity.name = 'nombre_de_actividad';
