7bdcc4981ea65d93a938bd2c979f0954929ae538
Daniel García <116081500+fregonaespanola@users.noreply.github.com>
2023-11-16 17:50:10 +0100

    cambios en planes y hecho detalle

---
+++ detalle.php
@@ -0,0 +1,111 @@
+<?php
+if(isset($_GET['id']) && is_numeric($_GET['id'])){
+    $id = $_GET['id'];
+}else{
+    //LOG
+    header('Location: 404.html');
+    die();
+}
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=pruebas','dani','1234');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $consulta = $db->prepare("SELECT * FROM Activity WHERE id = :id");
+    $consulta->bindParam(':id', $id, PDO::PARAM_INT);
+    $consulta->execute();
+    $plan = $consulta->fetch(PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+    echo "ERROR: " . $e->getMessage();
+    die();
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Planes Cercanos</title>
+    <meta charset="UTF-8" />
+    <meta name="author" content="Daniel García Ayala"/>
+    <meta name="description" content="Mapa"/>
+    <meta name="editor" content="Manual"/>
+    <meta name="keywords" lang="es" content=""/>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./styles_bt.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=iniciarMap" async defer></script>
+</head>
+<body>
+<?php
+require_once("header.php");
+?>
+    <!--Contenido-->
+<div class="container-fluid flex-grow-1 position-relative d-flex flex-column">
+    <div class="row">
+        <div class="col-md-12 bg-primary py-5 text-center background-image">
+            <div class="container">
+                 <h1 class="text-white"><?=$plan['name']?></h1>
+            </div>
+        </div>
+    </div>
+    
+
+     <!-- Sección de Descripción -->
+     <div class="row mt-4">
+        <div class="col-md-12">
+            <h2>Descripción</h2>
+            <?php if($plan['description']==NULL){?>
+                <p>No existe descripción para este plan.</p>
+            <?php
+            }else{?>
+            <p><?=$plan['description']?></p>
+            <?php
+            }?>
+        </div>
+    </div>
+
+    <!-- Sección de iconos y textos -->
+    <div class="row spacer">
+        <div class="col-md-12">
+        <img src="icono1.jpg" alt="Icono 1" class="img-fluid">
+        <span><?=$plan['place_name']?></span>
+        </div>
+    </div>
+
+    <div class="row spacer">
+        <div class="col-md-12">
+        <img src="icono2.jpg" alt="Icono 2" class="img-fluid">
+        <span><?=$plan['date']?></span>
+        </div>
+    </div>
+
+    <div class="row spacer">
+        <div class="col-md-12">
+        <img src="icono3.jpg" alt="Icono 3" class="img-fluid">
+        <span><?=$plan['time']?></span>
+        </div>
+    </div>
+
+    <!-- Sección de icono y texto adicionales -->
+    <div class="row">
+        <div class="col-md-12">
+        <img src="icono4.jpg" alt="Icono 4" class="img-fluid">
+        <span>Más información acerca del evento</span>
+        </div>
+    </div>
+</div>
+
+<?php
+require_once("footer.php");
+?>
+
+    <!-- Scripts -->
+    <script src="./script.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
--- planes.php
+++ planes.php
@@ -7,7 +7,7 @@ if(isset($_GET['evento'])){
 }
 
 if(isset($_GET['fecha']) && strlen($_GET['fecha']) === 10 && preg_match('/^[\d-]{10}$/', $_GET['fecha'])){
-    $fecha = strval(date('Y-m-d', strtotime(isset($_GET['fecha']))));
+    $fecha = $_GET['fecha'];
 }
 
 if(isset($_GET['page'])&& is_numeric($_GET['page']))
@@ -23,7 +23,7 @@ try{
 
     if($evento != NULL && $fecha!= NULL){
         $consulta = $db->prepare("
-        SELECT name, description, date, time, place_name 
+        SELECT id, name, description, date, time, place_name 
         FROM Activity 
         WHERE (name LIKE :evento OR category LIKE :evento) 
         AND date(date) = :fecha
@@ -36,7 +36,7 @@ try{
         $consulta->bindParam(':fecha', $fecha, PDO::PARAM_STR);
     }else if($evento != NULL && $fecha== NULL){
         $consulta = $db->prepare("
-        SELECT name, description, date, time, place_name 
+        SELECT id, name, description, date, time, place_name 
         FROM Activity 
         WHERE name LIKE :evento OR category LIKE :evento
         ORDER BY name 
@@ -47,7 +47,7 @@ try{
         $consulta->bindValue(':evento', "%$evento%", PDO::PARAM_STR);
     }else if($evento == NULL && $fecha!= NULL){
         $consulta = $db->prepare("
-        SELECT name, description, date, time, place_name 
+        SELECT id, name, description, date, time, place_name 
         FROM Activity 
         WHERE date = :fecha
         ORDER BY name 
@@ -55,9 +55,10 @@ try{
         OFFSET :offset
         ");
 
-$consulta->bindParam(':fecha', $fecha, PDO::PARAM_STR); 
+        $consulta->bindParam(':fecha', $fecha, PDO::PARAM_STR); 
     }else{
-        $consulta = $db->prepare("SELECT name, description, date, time, place_name FROM Activity ORDER BY name LIMIT :limite OFFSET :offset");
+        $consulta = $db->prepare("SELECT id, name, description, date, time, place_name FROM Activity ORDER BY name LIMIT :limite OFFSET :offset");
+        
     }
     $consulta ->bindValue(':limite',NUM_ELEM_POR_PAG, PDO::PARAM_INT);
     $consulta ->bindValue(':offset',NUM_ELEM_POR_PAG*($page-1), PDO::PARAM_INT);
@@ -93,92 +94,14 @@ $consulta->bindParam(':fecha', $fecha, PDO::PARAM_STR);
     <meta name="keywords" lang="es" content=""/>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="./styles_bt.css">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=iniciarMap" async defer></script>
 </head>
 <body>
-    <header class="bg-custom-color text-white">
-        <div class="d-flex justify-content-between align-items-center py-3">
-            <div class="logo ml-5">
-                <a href="#"><img src="./public/src/meetmap.png" alt="Logo de la empresa" height="50" style="margin-right: 50px;"></a>
-            </div>
-            <nav>
-                <ul class="nav mr-5">
-                    <li class="nav-item"><a href="#" class="nav-link text-white">Inicio</a></li>
-                    <li class="nav-item"><a href="planes.html" class="nav-link text-white">Planes Cerca</a></li>
-                    <li class="nav-item"><a href="planes.html" class="nav-link text-white">Categorías</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link text-white">FAQs</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link text-white">Contacto</a></li>
-                </ul>
-            </nav>
-            <div class="login mr-5">
-                <i class="fa fa-user"></i>
-                <a href="#" id="mostrarPopup" class="text-white" data-toggle="modal" data-target="#loginRegistroModal">Iniciar sesión</a>
-            </div>
-        </div>
-    </header>
-    
-    <!-- Modal -->
-   <!-- Modal único para iniciar sesión y registro -->
-<div class="modal fade" id="loginRegistroModal" tabindex="-1" role="dialog" aria-labelledby="loginRegistroModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header bg-custom-top text-white">
-                <h5 class="modal-title text-center w-100" id="loginRegistroModalLabel">Iniciar Sesión</h5>
-                <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body" style="background-color: #ECECEC;">
-                <div id="loginContent"> <!-- Contenido de inicio de sesión -->
-                    <form>
-                        <!-- Campos para iniciar sesión -->
-                        <div class="form-group">
-                            <label for="usernameLogin">Nombre de usuario</label>
-                            <input type="text" class="form-control" id="usernameLogin" placeholder="Ingrese su nombre de usuario">
-                        </div>
-                        <div class="form-group">
-                            <label for="passwordLogin">Contraseña</label>
-                            <input type="password" class="form-control" id="passwordLogin" placeholder="Ingrese su contraseña">
-                        </div>
-                        <p><a href="#" class="text-primary">¿Has olvidado tu contraseña?</a></p>
-                        <div class="text-center">
-                            <button type="submit" class="btn btn-custom-color btn-md text-white">Iniciar sesión</button>
-                        </div>
-                        <p class="mt-3 text-center">¿No tienes una cuenta? <a href="#" class="text-primary" onclick="mostrarRegistro()">Regístrate aquí</a></p>
-                    </form>
-                </div>
-                <div id="registroContent" style="display: none;"> <!-- Contenido de registro (inicialmente oculto) -->
-                    <form>
-                        <!-- Campos para registro -->
-                        <div class="form-group">
-                            <label for="emailRegistro">Correo electrónico</label>
-                            <input type="email" class="form-control" id="emailRegistro" placeholder="Ingrese su correo electrónico">
-                        </div>
-                        <div class="form-group">
-                            <label for="usernameRegistro">Nombre de usuario</label>
-                            <input type="text" class="form-control" id="usernameRegistro" placeholder="Ingrese su nombre de usuario">
-                        </div>
-                        <div class="form-group">
-                            <label for="passwordRegistro">Contraseña</label>
-                            <input type="password" class="form-control" id="passwordRegistro" placeholder="Ingrese su contraseña">
-                        </div>
-                        <div class="form-group">
-                            <label for="confirmPasswordRegistro">Repetir contraseña</label>
-                            <input type="password" class="form-control" id="confirmPasswordRegistro" placeholder="Repita su contraseña">
-                        </div>
-                        <div class="text-center">
-                            <button type="submit" class="btn btn-custom-color text-white">Registrarse</button>
-                        </div>
-                        <p class="mt-3 text-center">¿Ya tienes cuenta? <a href="#" class="text-primary" onclick="mostrarLogin()">Inicia sesión aquí</a></p>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
+<?php
+require_once("header.php");
+?>
     
     
 
@@ -259,12 +182,17 @@ $consulta->bindParam(':fecha', $fecha, PDO::PARAM_STR);
                             <div class="d-flex align-items-center">
                                 <img class="img-plan" src="./public/src/tree.jpg" alt="Plan">
                                 <div class="ml-3" style="padding-left: 0; padding-right: 0;">
-                                    <h2 class="mb-2 text-start"><?=$dato['name']?></h2>
+                                    <h2 class="mb-2 text-start"><a class="text-decoration-none text-dark" href="detalle.php?id=<?=$dato['id']?>"><?=$dato['name']?></a></h2>
                                     <?php
                                         // Limitar la descripción a tres líneas
                                         $description = $dato['description'];
                                         $descriptionLines = explode("\n", wordwrap($description, 220, "\n"));
-                                        $limitedDescription = implode("<br>", array_slice($descriptionLines, 0, 3));
+
+                                        // Limitar la descripción a tres líneas y agregar puntos suspensivos si hay más de tres líneas
+                                        $limitedDescription = implode("<br>", array_slice($descriptionLines, 0, 1));
+                                        if (count($descriptionLines) > 1) {
+                                            $limitedDescription .= '...';
+                                        }
                                     ?>
                                     <p class="mb-1 text-start"><?=$limitedDescription?></p>
                                     <p class="mb-1 text-start text-muted"><?=$dato['place_name']?></p>
@@ -278,24 +206,24 @@ $consulta->bindParam(':fecha', $fecha, PDO::PARAM_STR);
                     <?php if ($page > 1 && $page >= $num_pages - 2) { ?>
                         <span><a href="?page=1"><<</a></span>
                         <?php for ($i = max(1, $num_pages - 2); $i <= $num_pages; $i++) { ?>
-                            <span><a <?=($i == $page) ? "class='actual'" : "" ?> href="?page=<?=$i?>"><?=$i?></a></span>
+                            <span><a <?= ($i == $page) ? "class='actual'" : "" ?> href="?page=<?= $i ?>"><?= $i ?></a></span>
                         <?php } ?>
                         <?php if ($page < $num_pages) { ?>
-                            <span><a href="?page=<?=$num_pages?>">>></a></span>
+                            <span><a href="?page=<?= $num_pages ?>">>></a></span>
                         <?php } ?>
                     <?php } else if ($page <= 2) { ?>
                         <?php for ($i = 1; $i <= min(3, $num_pages); $i++) { ?>
-                            <span><a <?=($i == $page) ? "class='actual'" : "" ?> href="?page=<?=$i?>"><?=$i?></a></span>
+                            <span><a <?= ($i == $page) ? "class='actual'" : "" ?> href="?page=<?= $i ?>"><?= $i ?></a></span>
                         <?php } ?>
                         <?php if ($page < $num_pages) { ?>
-                            <span><a href="?page=<?=$num_pages?>">>></a></span>
+                            <span><a href="?page=<?= $num_pages ?>">>></a></span>
                         <?php } ?>
                     <?php } else { ?>
                         <?php for ($i = max(1, $page - 1); $i <= min($page + 1, $num_pages); $i++) { ?>
-                            <span><a <?=($i == $page) ? "class='actual'" : "" ?> href="?page=<?=$i?>"><?=$i?></a></span>
+                            <span><a <?= ($i == $page) ? "class='actual'" : "" ?> href="?page=<?= $i ?>"><?= $i ?></a></span>
                         <?php } ?>
                         <?php if ($page < $num_pages) { ?>
-                            <span><a href="?page=<?=$num_pages?>">>></a></span>
+                            <span><a href="?page=<?= $num_pages ?>">>></a></span>
                         <?php } ?>
                     <?php } ?>
                 </div>
@@ -305,12 +233,9 @@ $consulta->bindParam(':fecha', $fecha, PDO::PARAM_STR);
 
         
 </div>
-
-    <footer class="bg-custom-color text-white text-center">
-        <div class="container py-3">
-            <p>&copy; 2023 Meetmap</p>
-        </div>
-    </footer>
+<?php
+require_once("footer.php");
+?>
 
     <!-- Scripts -->
     <script src="./script.js"></script>
