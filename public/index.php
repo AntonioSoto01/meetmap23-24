9fd06a432cdac2647e5183311acf1350948313ca
Daniel García <116081500+fregonaespanola@users.noreply.github.com>
2023-11-21 17:42:50 +0100

    modificaciones en los markers para abrir el detalle

--- public/index.php
+++ public/index.php
@@ -15,8 +15,9 @@
      crossorigin=""/>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=iniciarMap" async defer></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@v0.79.0/dist/L.Control.Locate.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.4.1/MarkerCluster.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.4.1/MarkerCluster.Default.css" />
 
 </head>
 
@@ -41,6 +42,7 @@
      integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
      crossorigin=""></script>
      <script src="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@v0.79.0/dist/L.Control.Locate.min.js" charset="utf-8"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.4.1/leaflet.markercluster.js"></script>
      <script src="./map.js"></script>
 </body>
 </html>
--- public/map.js
+++ public/map.js
@@ -9,26 +9,50 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var markerIcon = L.icon({
     iconUrl: './images/marker_custom.png',
-    iconSize:     [38, 76], // size of the icon
-    iconAnchor:   [10, 76], // point of the icon which will correspond to marker's location
-    popupAnchor:  [10, -60] // point from which the popup should open relative to the iconAnchor
+    iconSize: [38, 76],
+    iconAnchor: [10, 76],
+    popupAnchor: [10, -60]
 });
 
-// Obtener datos de la base de datos
-fetch('./api/evento.php')
-  .then(response => response.json())
-  .then(data => {
-    // Iterar sobre los datos obtenidos y crear marcadores dinámicamente
-    data.forEach(activity => {
-      const latitude = activity.latitude;
-      const longitude = activity.longitude;
-      const name = activity.name;
+var markers = L.markerClusterGroup(); // Crea un grupo de marcadores
 
-      // Crear marcador dinámico y añadirlo al mapa
-      var marker = L.marker([latitude, longitude], { icon: markerIcon }).addTo(map);
-      marker.bindPopup(`<b>${name}</b>`).openPopup();
+// Obtener datos de la base de datos
+fetch('/api/evento.php')
+    .then(response => response.json())
+    .then(data => {
+        // Iterar sobre los datos obtenidos y crear marcadores dinámicamente
+        data.forEach(activity => {
+            const latitude = activity.latitude;
+            const longitude = activity.longitude;
+            const id = activity.id; // ID de la actividad
+            const name = activity.name;
+            const description = activity.description.substring(0, 100);
+            const date = activity.date;
+            const time = activity.time.substring(0, 5);;
+
+            // Crear marcador dinámico y añadirlo al grupo de marcadores
+            var marker = L.marker([latitude, longitude], { icon: markerIcon });
+
+            // Agregar contenido al popup
+            marker.bindPopup(`<a class="text-decoration-none" href="detalle.php?id=${id}">
+                <div class="titulo-marker">
+                    <strong>${name}</strong>
+                </div>
+                <div class="desc-marker">
+                    <p>${description}</p>
+                    <div class="fecha-marker">
+                        <div>${date}</div>
+                        <div>${time}</div>
+                    </div>
+                </div></a>`
+            );
+
+            markers.addLayer(marker); // Agregar el marcador al grupo
+        });
+
+        // Agregar el grupo de marcadores al mapa después de crear todos los marcadores
+        map.addLayer(markers);
+    })
+    .catch(error => {
+        console.error('Error al obtener datos:', error);
     });
-  })
-  .catch(error => {
-    console.error('Error al obtener datos:', error);
-  });
